a11e8312e7fda6b7bb15f3066580455ffd66e38d
salman-ibne-shofi <salmanibnshafi@gmail.com>
2025-02-19 14:34:06 +0600

    numbers array string array boolean array

---
+++ array.js
@@ -0,0 +1,11 @@
+// const num1 = 12;
+// const num2 = 22;
+// const num3 = 422;
+// const num4 = 422;
+
+// const numbers = 54685656545444;
+// (), {}, []
+const numbers = [12, 22, 54, 65]; // multiple numbers in a same variable
+const friends = ["Abul", "babul", "bulbul"]; // multiple string in a same variable
+const passed = [true, false, true, false]; // multiple boolean in a same variable
+const mixedArray = [12, "Alahabad", true, 45.566];
