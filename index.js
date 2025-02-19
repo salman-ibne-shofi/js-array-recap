53cb0b3f2ac3b16d5c903aa502cdea92efee2de0
salman-ibne-shofi <salmanibnshafi@gmail.com>
2025-02-19 20:14:12 +0600

    array index

---
+++ index.js
@@ -0,0 +1,17 @@
+const numbers = [88, 77, 89, 421, 54, 63]; // index start with zero(0)
+
+console.log(numbers);
+
+// get element value by index
+console.log(numbers[3]);
+
+const fourth = numbers[3]
+console.log(fourth);
+
+
+// set or update element value by index
+let abc = 45;
+abc = 99;
+
+numbers[1] = 999;
+console.log(numbers);
--- length.js
+++ length.js
@@ -1,2 +1,2 @@
-const numbers = [1, 5, 66, 55, 88, 77, 14, 48, 42, 79, 24, 15, 36];
+const numbers = [1, 5, 66, 55, 88, 77, 14, 48, 42, 79, 24, 15,];
 console.log(numbers.length);
