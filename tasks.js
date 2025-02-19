0fbb172d80d66932230fc63dce672f9c3746c993
salman-ibne-shofi <salmanibnshafi@gmail.com>
2025-02-19 23:18:05 +0600

    Add or remove elements from array tasks

--- tasks.js
+++ tasks.js
@@ -2,7 +2,16 @@
 const fruits = ["Mango", "Banana", "Guava", "Jackfruit", "Watermelon"];
 
 const favFruit = fruits[3];
-console.log(favFruit);
+// console.log(favFruit);
 
 fruits[2] = "Jambura";
-console.log(fruits);
+// console.log(fruits);
+
+// Add or remove elements
+const touristSpots = ["Bandarbon", "Sajek", "Cox Bazar"];
+touristSpots.push("Sundarban");
+console.log(touristSpots);
+touristSpots.push("Sugondha Beach", "Shatgombuj Mosquo");
+console.log(touristSpots);
+touristSpots.pop()
+console.log(touristSpots);
