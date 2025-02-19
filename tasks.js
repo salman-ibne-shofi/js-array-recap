af9ca245d31f7f5606267482857da1268a7ab8f3
salman-ibne-shofi <salmanibnshafi@gmail.com>
2025-02-19 23:08:14 +0600

    Declare an arry

---
+++ tasks.js
@@ -0,0 +1,8 @@
+// Declare an array
+const fruits = ["Mango", "Banana", "Guava", "Jackfruit", "Watermelon"];
+
+const favFruit = fruits[3];
+console.log(favFruit);
+
+fruits[2] = "Jambura";
+console.log(fruits);
